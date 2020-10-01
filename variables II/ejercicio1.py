3e7ade8ae94f8095c68aef53d5f2c84a5ab8adcd
Pamela <Richimetz>
2020-10-01 16:44:58 -0400

    agregar ejercicio1.py de variables II

---
+++ variables II/ejercicio1.py
@@ -0,0 +1,3 @@
+distanci_millas = float(input("ingrese las distancia en millas: "))
+distncia_kilometros = distanci_millas * 1.60934
+print (f"{distanci_millas} millas equivalen a {distncia_kilometros} kilometros")
