04bb0d175057c3d327bae36615d59fe5f8e00fde
Pamela <Richimetz>
2020-10-08 15:34:01 -0400

    agregar ejercicio1.pyy de ciclos

---
+++ ciclos/ejercicio1.py
@@ -0,0 +1,4 @@
+n = int(input("ingrese un número positivo: "))
+while n < 0:
+    print("error, intente con otro número")
+    n = int(input("ingrese otro número: "))
---
+++ flujo_condicional/ejer_2.py
@@ -0,0 +1,7 @@
+num1 = int(input("escriba un número: "))
+num2 = int(input("escriba un número: "))
+mensaje1 = "el numero mayor es: "
+if num1 < num2 :
+    print(f" el número mayor es: {num2}")
+if num1 > num2 :
+    print(f"el número mayor es: {num1}")
