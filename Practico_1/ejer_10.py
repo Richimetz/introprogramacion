ca2808b4d56ce04aed5963f306c31cab453f8286
Pamela <Richimetz>
2020-10-13 15:25:37 -0400

    agregar ejercicio4.py de ciclos

---
+++ Practico_1/ejer_10.py
@@ -0,0 +1,10 @@
+def timeConv (v):
+    n = int(input("cantidad de tramos realizados: "))
+    for i in range(0,n):
+        v = print("duración de tramo: ")
+        n = v % 60
+        print(n)
+        r = (v - m)/60
+        print(r)
+        resultado = str(r) + ":" + str(n)
+        print(timeConv(266))
