1566232cdd8e2fcf354bf43822739608092c134d
Pamela <Richimetz>
2020-10-13 10:44:03 -0400

    agregar ejer_1.py de practico_1

--- Practico_1/ejer_1.py
+++ Practico_1/ejer_1.py
@@ -1,5 +1,5 @@
 n = int(input("ingrese un número: "))
-if n%2 == 0:
-    print("su número es par ")
+if n % 2==0:
+    print("su número es par")
 else:
-    print("su número es impar ")
+    print("su número es impar")
