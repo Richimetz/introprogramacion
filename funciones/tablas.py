42134f2fcce61fd7ef98e7ca9abb99fc5a6b758c
Pamela <Richimetz>
2020-10-22 16:21:04 -0400

    agregar tablas.py de funciones

---
+++ funciones/tablas.py
@@ -0,0 +1,10 @@
+#tablas de multiplicar
+def tablaM():
+    for r in range (1,11):
+        print("La tabla del número: ", r)
+        for i in range (1,11):
+            print(r, "x", i, "=", i*r)
+tablaM()
+
+
+
