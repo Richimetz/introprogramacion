f0fc8f004583c5227c1dffefa660500bd4195c3c
Richimetz <richi.metzelar.riffarach@gmail.com>
2020-11-23 15:35:22 -0400

    agregar ejer_1vec.py

---
+++ vectores/ejer_2vec.py
@@ -0,0 +1,12 @@
+vect1 = []
+vect2 = []
+for i in range (1,5+1):
+    lol = input("ingrese la lista 1: ")
+    vect1.append(lol)
+for i in range (1,5+1):
+    xd = input("ingrese la lista 2: ")
+    vect2.append(xd)
+print(vect1, vect2)
+print("vector resultado:", (vect1 + vect2) )
+
+
